49ff1c84ac97bebb78adec521350227ae33e78b2
Michael Pistrang <michael@pistrang.com>
2021-05-23 12:12:01 -0400

    site content

--- site-content/map.js
+++ site-content/map.js
@@ -1,7 +1,7 @@
 // set up base layers
 const mbAttr =
     'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a> (MCP)',
   mbUrl =
     "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWljaGFlbHBpc3RyYW5nIiwiYSI6ImNrb3ozOWV5NTBncXEydmxxdmc5dWhlNGEifQ.ZN7YsaG1SRwuaZ_GH00Zvg";
 
