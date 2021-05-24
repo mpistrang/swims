494c6c7a8f4647f84beae4b862e0043ff1876871
Michael Pistrang <michael@pistrang.com>
2021-05-23 21:20:03 -0400

    use spreadsheet

--- build_geojson.py
+++ build_geojson.py
@@ -1,11 +1,37 @@
-import argparse
-import csv
 import json
+import os
 
-TAB_DELIMITER = '	'
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+
+def get_data_from_google_sheet():
+    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=os.environ['DAVID_SWIMS_SPREADSHEET_ID'],
+                                range=os.environ['DAVID_SWIMS_DATA_RANGE']).execute()
+    values = result.get('values', [])
+
+    headers = values.pop(0)
+
+    for row in values:
+        yield {k: v for k, v in zip(headers, row)}
 
 
 def convert_row_to_geojson(row):
+
     lat, lon = row['Coordinates'].split(",")
     swim_geojson = {
         "type": "Feature",
@@ -23,21 +49,13 @@ def convert_row_to_geojson(row):
     return swim_geojson
 
 
-def get_swims_geojson(filename):
-    with open(filename, 'r') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=TAB_DELIMITER)
-        for row in reader:
-            yield convert_row_to_geojson(row)
+def get_swims_geojson():
+    yield from (convert_row_to_geojson(row) for row in get_data_from_google_sheet())
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description='Convert tab-seperated tsv into geojson.')
-    parser.add_argument('filename', type=str,
-                        help='Filename of the .tsv to load')
-    args = parser.parse_args()
-
-    swims_geojson = get_swims_geojson(args.filename)
+    raw_data = get_data_from_google_sheet()
+    swims_geojson = (convert_row_to_geojson(row) for row in raw_data)
 
     geojson = {
         "type": "FeatureCollection",
